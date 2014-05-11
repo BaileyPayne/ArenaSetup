d8b03798a53907220dba3ff84ba9f7902350fd4b
Bailey Payne <Bailpy6@aol.co.uk>
2014-05-11 10:23:50 +0100

    Update ArenaSetupCommands.java

--- src/me/baileypayne/arenasetup/ArenaSetupCommands.java
+++ src/me/baileypayne/arenasetup/ArenaSetupCommands.java
@@ -18,11 +18,56 @@ public class ArenaSetupCommands {
         //create arena + set name
         if(player.hasPermission("arena.create")){
                     if(commandLabel.equalsIgnoreCase("create")){
-                        plugin.setName(args[0]);
+                        ArenaManager.getManager().createArena(args[0], null, null, null, null, 10);
                         player.sendMessage("You Created the Arena named " + args[0]);
                     }
                 }
-        //add rest of creating arena commands here
+        //set max players
+        //if(player.hasPermission("arena.set.maxplayers")){
+           //if(commandLabel.equalsIgnoreCase("setmaxp")){
+               // if(args[0] == plugin.getName()){
+                  //  plugin.setMaxPlayers(args[1]);
+                    player.sendMessage("You set the Max Players to " + args[1] + " in " + plugin.getName() + " Arena!");
+             //   }
+         //   }
+    //    }
+        //set lobby location
+        if(player.hasPermission("arena.set.lobby")){
+            if(commandLabel.equalsIgnoreCase("setlobbyspawn")){
+                //check that the arena exist
+                ArenaManager.getManager().getArena(args[0]).setLobbyLocation(player.getLocation());
+                //sends confirmation message
+                player.sendMessage("Lobby Location has been succesfully added!");
+                
+            }
+        }
+        //set red spawn
+        if(player.hasPermission("arena.set.redspawn")){
+            if(commandLabel.equalsIgnoreCase("setredspawn")){
+                //Check that the arena is equal to the arguements and sets spawn
+                ArenaManager.getManager().getArena(args[0]).setRedSpawn(player.getLocation());
+                //sends confirmation message
+                player.sendMessage("Red Spawn has been succesfully added!");
+            }
+        }
+        //set blue spawn
+        if(player.hasPermission("arena.set.bluespawn")){
+            if(commandLabel.equalsIgnoreCase("setbluespawn")){
+                //check that the arena is equal to the arguements plus set spawn
+                ArenaManager.getManager().getArena(args[0]).setBlueSpawn(player.getLocation());
+                //sends confirmation message
+                player.sendMessage("Blue Spawn has been succesfully added!");
+            }
+        }
+        //set end location
+        if(player.hasPermission("arena.set.endloc")){
+            if(commandLabel.equalsIgnoreCase("setendloc")){
+                //check the arena is real and set spawn
+                ArenaManager.getManager().getArena(args[0]).setEndLocation(player.getLocation());
+                //sends confirmation message
+                player.sendMessage("End Location Succesfully added!");
+            }
+        }
         return false;
     }
 }

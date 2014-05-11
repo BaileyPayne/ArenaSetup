5e8919eda1b3d95ec22285b88b9d966f388cfec5
Bailey Payne <Bailpy6@aol.co.uk>
2014-05-11 15:38:52 +0100

    Update ArenaSetup.java

--- src/me/baileypayne/arenasetup/ArenaSetup.java
+++ src/me/baileypayne/arenasetup/ArenaSetup.java
@@ -1,10 +1,6 @@
 
 package me.baileypayne.arenasetup;
 
-import java.io.File;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -16,6 +12,7 @@ public class ArenaSetup extends JavaPlugin {
     public void onEnable(){
         ArenaManager.getManager().loadArenas();
         loadConfigFile();
+        
                 
     }
     //random number generator
@@ -33,36 +30,5 @@ public class ArenaSetup extends JavaPlugin {
 		getConfig().options();
 		saveConfig();
 	}
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-                Player player = (Player) sender;
-                
-                //Player Commands
-                
-                //join
-                if(commandLabel.equalsIgnoreCase("join")){
-                    ArenaManager.getManager().addPlayers(player, args[0]);
-                }
-                //leave
-                if(commandLabel.equalsIgnoreCase("leave")){
-                    ArenaManager.getManager().removePlayer(player, args[0]);
-                }
-                
-                //Admin Commands
-                
-                //start
-                if(player.hasPermission("arena.start")){
-                    if(commandLabel.equalsIgnoreCase("forcestart")){
-                        ArenaManager.getManager().startArena(args[0]);
-                    }
-                }
-                //stop
-                if(player.hasPermission("arena.end")){
-                    if(commandLabel.equalsIgnoreCase("forcestop")){
-                        ArenaManager.getManager().endArena(args[0]);
-                    }
-                }
-                                
-        return false;
-
-    }            
+               
 }

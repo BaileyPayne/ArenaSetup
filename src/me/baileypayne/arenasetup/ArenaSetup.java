0ee25b339b802a664585be86b30d580d54c407c0
Bailey Payne <Bailpy6@aol.co.uk>
2014-05-11 10:23:24 +0100

    Update ArenaSetup.java

--- src/me/baileypayne/arenasetup/ArenaSetup.java
+++ src/me/baileypayne/arenasetup/ArenaSetup.java
@@ -2,6 +2,9 @@
 package me.baileypayne.arenasetup;
 
 import java.io.File;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -12,7 +15,8 @@ public class ArenaSetup extends JavaPlugin {
     
     public void onEnable(){
         ArenaManager.getManager().loadArenas();
-        
+        loadConfigFile();
+                
     }
     //random number generator
     public static int randint(){
@@ -24,4 +28,41 @@ public class ArenaSetup extends JavaPlugin {
 		int randomInt = (int)d+1;
 		return randomInt;
 	}
+    private void loadConfigFile() {
+		saveDefaultConfig();
+		getConfig().options();
+		saveConfig();
+	}
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+                Player player = (Player) sender;
+                
+                //Player Commands
+                
+                //join
+                if(commandLabel.equalsIgnoreCase("join")){
+                    ArenaManager.getManager().addPlayers(player, args[0]);
+                }
+                //leave
+                if(commandLabel.equalsIgnoreCase("leave")){
+                    ArenaManager.getManager().removePlayer(player, args[0]);
+                }
+                
+                //Admin Commands
+                
+                //start
+                if(player.hasPermission("arena.start")){
+                    if(commandLabel.equalsIgnoreCase("forcestart")){
+                        ArenaManager.getManager().startArena(args[0]);
+                    }
+                }
+                //stop
+                if(player.hasPermission("arena.end")){
+                    if(commandLabel.equalsIgnoreCase("forcestop")){
+                        ArenaManager.getManager().endArena(args[0]);
+                    }
+                }
+                                
+        return false;
+
+    }            
 }

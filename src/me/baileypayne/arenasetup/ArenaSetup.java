3304a5fe7b431941ada0f6f645deb75bb522439d
Bailey Payne <Bailpy6@aol.co.uk>
2014-05-20 20:50:18 +0100

    Update ArenaSetup.java

--- src/me/baileypayne/arenasetup/ArenaSetup.java
+++ src/me/baileypayne/arenasetup/ArenaSetup.java
@@ -30,5 +30,99 @@ public class ArenaSetup extends JavaPlugin {
 		getConfig().options();
 		saveConfig();
 	}
+	
+	@Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        Player player = (Player) sender;
+        //Player Commands
+        //join an arena
+        if(commandLabel.equalsIgnoreCase("join")){
+            if(args.length == 1){
+                ArenaManager.getManager().addPlayers(player, args[0]);
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+        //leave an arena
+        if(commandLabel.equalsIgnoreCase("leave")){
+            if(args.length == 1){
+                ArenaManager.getManager().removePlayer(player, args[0]);
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+        //Admin Commands
+        //force start
+        if(commandLabel.equalsIgnoreCase("forcestart")){
+            if(args.length == 1){
+                ArenaManager.getManager().startArena(args[0]);
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+        //force stop
+        if(commandLabel.equalsIgnoreCase("forcestop")){
+            if(args.length == 1){
+                ArenaManager.getManager().endArena(args[0]);
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+        //create
+        if(commandLabel.equalsIgnoreCase("create")){
+            if(args.length == 2){
+                int i = Integer.parseInt(args[1]);
+                ArenaManager.getManager().createArena(args[0], i);
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+        //set lobby location
+        if(commandLabel.equalsIgnoreCase("setlobbyspawn")){
+            if(args.length == 1){
+                ArenaManager.getManager().setLobbyLocation(args[0], player.getLocation());
+                player.sendMessage("Set Lobby Location!");
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+        //set red spawn
+        if(commandLabel.equalsIgnoreCase("setredspawn")){
+            if(args.length == 1){
+                ArenaManager.getManager().setRedSpawn(args[0], player.getLocation());
+                player.sendMessage("Set Red Spawn!");
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+        //set blue spawn
+        if(commandLabel.equalsIgnoreCase("setbluespawn")){
+            if(args.length == 1){
+                ArenaManager.getManager().setBlueSpawn(args[0], player.getLocation());
+                player.sendMessage("Set Blue Location!");
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+        //set end loc
+        if(commandLabel.equalsIgnoreCase("setendloc")){
+            if(args.length == 1){
+                ArenaManager.getManager().setEndLocation(args[0], player.getLocation());
+                player.sendMessage("Set End Location!");
+            }
+            else{
+                player.sendMessage("Insufficient Arguments!");
+            }
+        }
+                return false;
+    }
                
 }
